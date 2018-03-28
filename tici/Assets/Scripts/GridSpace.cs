b5a11a3f7746d571c22622f03d70a2ab4a238173
CthulhuTheKing <37659836+CthulhuTheKing@users.noreply.github.com>
2018-03-28 15:48:45 +0200

    Hackerman is our lord and saviour

---
+++ tici/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+
+    private GameController gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+}

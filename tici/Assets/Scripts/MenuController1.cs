36b5234c6e116c4465746b9cec8ac1f3a47ec685
Flargy <marlun1996@hotmail.com>
2018-03-29 11:47:00 +0200

    Ny kod
    menucotnroller

---
+++ tici/Assets/Scripts/MenuController1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController1 : MonoBehaviour {
+
+    public GameObject tutorialText;
+
+    [SerializeField]
+    public int levelToLoad;
+
+    public void Start()
+    {
+        tutorialText.SetActive(false);
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void tutorial()
+    {
+        
+        tutorialText.SetActive(!tutorialText.activeSelf);
+        
+    }
+}
